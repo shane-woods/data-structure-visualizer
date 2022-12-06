42b1de8503003ea852a64c27c33a184628136d7d
Shane <shwoods35@gmail.com>
2022-12-05 22:16:16 -0500

    render any size array done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8c1861a0b7e6a58"; }
+/******/ 		__webpack_require__.h = function() { return "aca56e1ecd8d16a9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/Array.js
+++ pages/Array.js
@@ -2,41 +2,142 @@ import React, { useState, useEffect, useCallback} from 'react'
 import styles from '../styles/Array.module.css'
 import Navbar from '../components/navbar';
 
-let array = [];
+let initialArray = [
+  {value:1,},
+  {value:2},
+  {value:3},
+  {value:4},
+  {value:5},
+]
 
 const Cell = (props) => {
   let value = props.value;
-  let id = props.id;
+  let index = props.index;
+  const style = {
+    display: "flex",
+    borderRadius: "1px",
+    textAlign: "center",
+    padding:"25px",
+    fontSize: "large"
+  }
   return (
-    <div className={styles.cell} key={id}>
+    <div key={index} style={style}>
       {value}
     </div>
   );
 }
 
 const Arr = (props) => {
-  let size = props.size;
-  array = Array.from({length:size}, () => 1);
+  let array = props.array;
   return (
-    <div className={styles.array} key="array">
+    <div className={styles.array}>
       {
-        array.map((_,i) => <Cell value={Math.floor(Math.random() * 50)} id={i}/>)
+        array.map((cell, index) => (
+          <Cell value={cell.value} index={index}/>
+        ))
       }
     </div>
   );
 }
 
-const Form = () => {
-  const [size, setSize] = useState(5);
-  const [isSubmitted, showArray] = useState(false);
+const OperationField = (props) => {
+  let form = props.form;
+  let setForm = props.onOperation;
+  return (
+    <label>
+      Operation:
+      <select 
+      name="operation" 
+      id="operation_select"
+      onChange={e => {
+        setForm({
+          ...form,
+          operation: e.target.value
+        });
+      }}
+      >
+        <option value="">Select an Operation</option>
+        <option value="search">Search</option>
+        <option value="add">Add</option>
+        <option value="delete">Delete</option>
+      </select>
+    </label>
+  );
+}
 
-  useEffect(() =>  {
-    showArray(true);
-  }, [])
+const SizeField = (props) => {
+  let form = props.form;
+  let setForm = props.onSize;
+  return (
+    <label>
+        Size: 
+        <input 
+          type="text" 
+          id="size" 
+          name="array_size" 
+          value={form.size}
+          onChange={e => {
+            setForm({
+              ...form,
+              size: e.target.value
+            });
+          }}
+        />
+    </label>
+  );
+}
+
+const Form = () => {
+  const [array, setArray] = useState(initialArray);
+  const [form, setForm] = useState({
+    size: 5,
+    operation: 'choose an operation'
+  })
+  const [submitted, setSubmission] = useState(false);
+
+  function handleClick() {
+    setArray(randomArray);
+    setSubmission(true);
+  }
+
+  function performOperation(operation) {
+    switch (operation) {
+      case "search":
+        console.log("Search");
+      case "add":
+        console.log("Search");
+      case "delete":
+        console.log("Delete");
+      default:
+        console.log("Choose an operation");
+    }
+  }
+
+  function randomArray() {
+    array = [];
+    for (let i = 0; i < form.size; i++) {
+      let cell = {
+        value : Math.floor(Math.random() * 50),
+        color : "black"
+      }
+      array.push(cell);
+    }
+    return array;
+  }
+
+  function submitForm(event) {
+    event.preventDefault();
+  }
 
   return (
     <div className={styles.ArrayPage}>
-      {isSubmitted ? <Arr size={size}/> : <Arr size={0}/>}
+    <form onSubmit={submitForm}>
+      <SizeField form={form} onSize={setForm} />
+      <OperationField form={form} onOperation={setForm} />
+      <button type="button" name="execute" onClick={handleClick}>Execute</button>
+    </form>
+    {form.operation === "choose an operation" ? <Arr array={array}/> : null}
+    <p>{form.size}{' '}{form.operation}</p>
     </div>
   );
 }
